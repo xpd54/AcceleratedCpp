cf03fe2183337d9e001b2bb605f2519bff625fc7
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-05-10 12:00:44 +0800

    Start with google benchmark

--- WorkingWithMoveSemantic/main.cpp
+++ WorkingWithMoveSemantic/main.cpp
@@ -30,9 +30,24 @@ public:
 
   Article &operator=(Article &&rhs) {
     m_data = std::move(rhs.m_data);
-    rhs.m_data = nullptr;
     return *this;
   }
+
+  Article &operator=(Article &rhs) {
+    m_data = std::move(rhs.m_data);
+    return *this;
+  }
+
+  Article(const Article &other) : m_data(other.m_data) {
+    std::cout << "<---Copy Constructor Called--->"
+              << "\n";
+  }
+
+  Article(Article &&other) : m_data(std::move(other.m_data)) {
+    std::cout << "<---Move Constructor Called--->" << "\n";
+    other.m_data.clear();
+  }
+
 
   const std::string &get_content() const { return m_data; };
   void set_content(const std::string &content);
@@ -45,11 +60,18 @@ std::string getEveningNews() {
   return std::string("Hello how are you this evening?");
 }
 
+Article getArticle() {
+  Article hello_to_local("Hello To local");
+  return hello_to_local;
+}
+
 int main() {
   std::string hello("hello");
   Article news(getNews());
   Article evening_news(getEveningNews());
   Article *morning_news = new Article(hello);
   std::cout << morning_news->get_content() << "\n";
+  Article document;
+  document = getArticle();
   return 0;
 }
