9a1dbcef506befe723d996bdcbdb14672fd0aa5b
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-05-25 19:06:50 +0800

    Add readwrite via mmap

---
+++ workingWithOops/readingDataToViertualMemory.cpp
@@ -0,0 +1,26 @@
+#include <chrono>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/mman.h>
+#include <unistd.h>
+#define FILE_PATH "output.dat"
+struct CSVData {
+  std::chrono::system_clock::time_point time;
+  double value;
+  double index;
+};
+
+int main() {
+  CSVData holder;
+  size_t size = sizeof(holder);
+  int input = open(FILE_PATH, O_RDONLY);
+  void *point = mmap(0, size, PROT_READ, MAP_SHARED, input, 0);
+  CSVData *data = (CSVData *)point;
+  double value = 10978.4889L;
+  std::cout << std::fixed << std::setprecision(6);
+  std::cout << data[0].value << "\n";
+  close(input);
+  return 0;
+}
---
+++ workingWithOops/writingDataToVirtualMemory.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/mman.h>
+#include <unistd.h>
+#define FILE_PATH "output.dat"
+struct CSVData {
+  std::chrono::system_clock::time_point time;
+  double value;
+  double index;
+};
+
+int main() {
+  CSVData holder;
+  holder.time = std::chrono::system_clock::now();
+  holder.value = 10978.4889L;
+  holder.index = 10.2;
+  size_t size = sizeof(holder);
+  int output = open(FILE_PATH, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+  truncate(FILE_PATH, size);
+  CSVData *point =
+      (CSVData *)mmap(nullptr, size, PROT_WRITE, MAP_SHARED, output, 0);
+  point[0] = holder;
+  close(output);
+  std::cout << sizeof(holder) << "\n";
+  return 0;
+}
