f794854d53ef9c7a10493bf13528e225ebb12612
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-06-14 07:53:24 +0800

    Add sales data for abstract class

---
+++ workingWithAbstractDataTypes/salesData.cpp
@@ -0,0 +1,28 @@
+#include "salesData.h"
+#include <iostream>
+double Sales_data::avg_price() const {
+  if (units_sold)
+    return revenue / units_sold;
+  else
+    return 0;
+}
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+  units_sold += rhs.units_sold;
+  revenue += rhs.revenue;
+  return *this;
+}
+
+std::istream &read(std::istream &input_stream, Sales_data &item) {
+  double price = 0;
+  input_stream >> item.bookNo >> item.units_sold >> price;
+  item.revenue = price * item.units_sold;
+  return input_stream;
+}
+
+std::ostream &print(std::ostream &output_stream, const Sales_data &item) {
+  output_stream << item.isbn() << " " << item.units_sold << " " << item.revenue
+                << " " << item.avg_price();
+  return output_stream;
+}
+
