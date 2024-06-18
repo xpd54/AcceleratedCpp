754e033a4ed97827fdd6b0c097050f7dc86c72ee
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-06-19 06:20:18 +0800

    Add implicit vs explicit get and move method

--- workingWithAbstractDataTypes/main.cpp
+++ workingWithAbstractDataTypes/main.cpp
@@ -1,10 +1,18 @@
 #include "salesData.h"
+#include "screen.h"
 #include <iostream>
 #include <string>
 int main() {
+  /*
   Sales_data default_value;
   Sales_data cin_vlaue(std::cin);
   print(std::cout, default_value) << "\n";
   print(std::cout, cin_vlaue) << "\n";
+*/
+  Screen clientScreen;
+  char ch = clientScreen.get();
+  std::cout << ch << "\n";
+  std::cout << "with over loaded " << clientScreen.get(0, 0) << "\n";
+
   return 0;
 }
--- workingWithAbstractDataTypes/screen.h
+++ workingWithAbstractDataTypes/screen.h
@@ -2,13 +2,33 @@
 class Screen {
 public:
   typedef std::string::size_type pos;
-  Screen(/* args */);
-  ~Screen();
+  Screen() = default;
+  Screen(pos ht, pos wd, char c)
+      : height(ht), width(wd), contents(ht * wd, c){};
+  char get() const {
+    return contents[cursor];
+  } // implicitly inline as it's defined inline
+  inline char get(pos ht, pos wd)
+      const; // explicit inline means it will get inline when it's get defined
+  Screen &move(pos r, pos c); // this can be make inline while making defination
+  
 
 private:
   pos cursor = 0;
   pos height = 0, width = 0;
   std::string contents;
 };
+
+inline Screen &Screen::move(pos r, pos c) { // r is row and c is column
+  pos row = r + width;                      // compute the row location
+  cursor = row + c;
+  return *this;
+}
+
+// get is inine here as it's in declaration
+char Screen::get(pos r, pos c) const {
+  pos row = r + width; // compute row location
+  return contents[row + c]; // return character at the given column
+}
 
 
